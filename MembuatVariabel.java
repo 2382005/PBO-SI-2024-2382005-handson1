8645724154ea16f66d1386aa1d742d7c67109b5d
Gabriel Blessing Christian Hutagaol <2381002@unai.edu>
2024-09-03 10:23:16 +0700

    Handson 1

---
+++ MembuatVariabel.java
@@ -0,0 +1,10 @@
+public class MembuatVariabel {
+    public static void main(String[] args) {
+        int tipeDataInteger;
+        tipeDataInteger = 20;
+        long tipeDataLong = 30L;
+        long showInfo;
+        final String FULL_NAME = "RACHEL RATNA SARI";
+    }
+}
+
---
+++ TipeDataArray.java
@@ -0,0 +1,10 @@
+public class TipeDataArray {
+    public static void main(String[] args){
+        int[] kumpulanAngka = new int [3];
+        kumpulanAngka[0] = 10;
+        kumpulanAngka[1] = 40;
+        kumpulanAngka[2] = 50;
+        System.out.println(kumplanAngka[2]);
+        int[][]
+    }
+}
